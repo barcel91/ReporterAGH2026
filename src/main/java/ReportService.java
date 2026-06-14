f3c537c49b15af9a70bf129e18863fae960beeea
gomolkap <pgomolka@student.agh.edu.pl>
2026-06-14 10:11:46 +0200

    Dodanie do klasy ReportService agregowania danych dla raportów 3 i 4

--- src/main/java/ReportService.java
+++ src/main/java/ReportService.java
@@ -36,5 +36,65 @@ public class ReportService {
                 })
                 .collect(Collectors.toList());
     }
+
+    public Collection<ReportData> employeeProjects(
+            Collection<Task> tasks,
+            String user) {
+
+        double totalHours = tasks.stream()
+                .filter(task -> user.equals(task.getUser()))
+                .mapToDouble(Task::getDuration)
+                .sum();
+
+        return tasks.stream()
+                .filter(task -> user.equals(task.getUser()))
+                .collect(Collectors.groupingBy(
+                        Task::getProject,
+                        Collectors.summingDouble(Task::getDuration)
+                ))
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    ReportData data = new ReportData();
+                    data.setUser(user);
+                    data.setProject(entry.getKey());
+                    data.setDuration(entry.getValue());
+
+                    if (totalHours > 0) {
+                        data.setPercentage(
+                                entry.getValue() * 100.0 / totalHours
+                        );
+                    }
+
+                    return data;
+                })
+                .collect(Collectors.toList());
+    }
+
+    public Collection<ReportData> tasksInProject(
+            Collection<Task> tasks,
+            String project) {
+
+        return tasks.stream()
+                .filter(task -> project.equals(task.getProject()))
+                .collect(Collectors.groupingBy(
+                        Task::getTask,
+                        Collectors.summingDouble(Task::getDuration)
+                ))
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    ReportData data = new ReportData();
+                    data.setProject(project);
+                    data.setTask(entry.getKey());
+                    data.setDuration(entry.getValue());
+                    return data;
+                })
+                .sorted((a, b) ->
+                        Double.compare(
+                                b.getDuration(),
+                                a.getDuration()))
+                .collect(Collectors.toList());
+    }
 }
 
